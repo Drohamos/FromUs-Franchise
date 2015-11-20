e1ebf7117de3f6a67bb252f4e453ef6508625d14
Drohamos <barkasrobin@gmail.com>
2015-11-20 17:30:45 +0100

    Création de la page qui sommes-nous

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 		<nav id="mainMenu" class="pure-menu pure-menu-horizontal">
 			<a href="index.php" id="logo" class="pure-menu-heading"><img src="images/logo.png" alt=""></a>
 			<ul class="pure-menu-list">
-				<li class="pure-menu-item"><a href="#" class="pure-menu-link">Qui sommes-nous ?</a></li>
+				<li class="pure-menu-item"><a href="qui-sommes-nous.php" class="pure-menu-link">Qui sommes-nous ?</a></li>
 				<li class="pure-menu-item"><a href="devenir-franchise.php" class="pure-menu-link">Devenir franchisé</a></li>
 				<li class="pure-menu-item"><a href="#" class="pure-menu-link">Notre implantation</a></li>
 				<li class="pure-menu-item"><a href="definir-projet.php" class="pure-menu-link">Définir votre projet</a></li>
---
+++ qui-sommes-nous.php
@@ -0,0 +1,29 @@
+<?php include('header.php'); ?>
+
+	<article class="pure-g" id="defineProjectIntro">
+		<section class="pure-u-1 pure-u-lg-3-5">
+			<p>From-US.com est une entreprise française dynamique, spécialisée dans l’e-service transfrontalier dont le siège est basé dans la ville de Mulhouse en Alsace, en France. La filiale américaine de la société se trouve quant à elle à Miami, dans l’état de Floride aux Etats-Unis, état où les taxes d’achats et les couts logistiques pour le consommateur sont très compétitifs. L’entreprise a été créée en 2011 et prône des valeurs telles que la confiance, la qualité et la disponibilité.</p>
+			<p>Aujourd’hui, From-US.com rend service à une clientèle mondiale grâce à l’espace et son support client multilingue. Puisque pour nous, il n’y a pas de frontières.</p>
+		</section>
+		<section class="pure-u-1 pure-u-lg-2-5" style="text-align: center">
+			<div><iframe class="pure-img" style="width: 100%; height: auto;" src="https://www.youtube.com/embed/O6Nl8RmmlWM" frameborder="0" allowfullscreen></iframe></div>
+		</section>
+	</article>
+
+<article class="iconsWithDesc">
+	<header><h2 class="xxlarge titleWithUnderline">Le concept de From-US.com</h2></header>
+	<p>Notre concept, basé sur l’importation de produits américains, a pour objectif d’ouvrir les futurs clients à la culture américaine, aux produits et références américaines, et de réduire au maximum la distance séparant les deux continents. From-US.com est ainsi un facilitateur prestataire de services dans l’e-commerce transfrontalier. Commander aux USA avec From-US.com, c’est avoir un gage de sécurité et d’efficacité tout en faisant des économies.</p>
+	<p>Aujourd’hui, From-US.com rend service à une clientèle mondiale grâce à l’espace et son support client multilingue. Puisque pour nous, il n’y a pas de frontières.</p>
+	<ul class="pure-g">
+		<li class="pure-u-1 pure-u-md-1-2" style="background-image: url(images/icon_aventure.png)">
+			<h3 class="red large">Le service concierge</h3>
+			<p>Le service concierge facilite l’achat de produits US à beaucoup de client ! Effectivement, il n’a qu’à choisir les produits qu’il souhaite et nous passons commande pour lui, sur le site US de son choix.</p>
+		</li>
+		<li class="pure-u-1 pure-u-md-1-2" style="background-image: url(images/icon_aventure.png)">
+			<h3 class="red large">Le service notification</h3>
+			<p>Grâce à ce service, vous pouvez passer directement votre commande sur le site US de votre choix, From-US.com se charge de la livraison, où que vous soyez dans le monde.</p>
+		</li>
+	</ul>
+</article>
+
+<?php include('footer.php'); ?>
